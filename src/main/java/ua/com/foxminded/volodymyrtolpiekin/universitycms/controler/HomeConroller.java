5eabff992a314f6540ef5b5052514deeb3c27e71
Volodymyr Tolpiekin <tolpekin.vladimir@gmail.com>
2022-11-28 17:14:47 +0200

    Added missed access modifier.

--- src/main/java/ua/com/foxminded/volodymyrtolpiekin/universitycms/controler/HomeConroller.java
+++ src/main/java/ua/com/foxminded/volodymyrtolpiekin/universitycms/controler/HomeConroller.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeConroller {
 
     @Value("${spring.application.name}")
-    String appName;
+    private String appName;
 
     @GetMapping("/")
     public String homePage(Model model) {
