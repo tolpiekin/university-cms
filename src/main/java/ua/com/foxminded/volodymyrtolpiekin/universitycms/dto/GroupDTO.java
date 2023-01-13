5f6ca7e87f4bd9c0e97e8854e9ca0638ae287ec4
Volodymyr Tolpiekin <tolpekin.vladimir@gmail.com>
2023-01-13 16:11:03 +0200

    Added GroupDTO

---
+++ src/main/java/ua/com/foxminded/volodymyrtolpiekin/universitycms/dto/GroupDTO.java
@@ -0,0 +1,13 @@
+package ua.com.foxminded.volodymyrtolpiekin.universitycms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupDTO {
+    private Long id;
+    private String name;
+}
