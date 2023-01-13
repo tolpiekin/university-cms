e51f42d670be09265e32f602fe3a4cf1bb432fb7
Volodymyr Tolpiekin <tolpekin.vladimir@gmail.com>
2023-01-13 16:12:39 +0200

    Service methods for GroupDTO implemented.

--- src/main/java/ua/com/foxminded/volodymyrtolpiekin/universitycms/service/impl/GroupServiceImpl.java
+++ src/main/java/ua/com/foxminded/volodymyrtolpiekin/universitycms/service/impl/GroupServiceImpl.java
@@ -1,21 +1,27 @@
 package ua.com.foxminded.volodymyrtolpiekin.universitycms.service.impl;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import ua.com.foxminded.volodymyrtolpiekin.universitycms.dto.GroupDTO;
 import ua.com.foxminded.volodymyrtolpiekin.universitycms.models.Group;
 import ua.com.foxminded.volodymyrtolpiekin.universitycms.repository.GroupRepository;
 import ua.com.foxminded.volodymyrtolpiekin.universitycms.service.GroupService;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 @Service
 public class GroupServiceImpl implements GroupService {
 
     private final GroupRepository groupRepository;
+    private final ModelMapper mapper;
 
-    public GroupServiceImpl(GroupRepository groupRepository) {
+    public GroupServiceImpl(GroupRepository groupRepository, ModelMapper mapper) {
         this.groupRepository = groupRepository;
+        this.mapper = mapper;
     }
 
     @Override
@@ -43,5 +49,25 @@ public class GroupServiceImpl implements GroupService {
     public void deleteById(Long id){
         findById(id);
         groupRepository.deleteById(id);
+    }
+
+    @Override
+    public List<GroupDTO> readAllDTOs() {
+        return findAll()
+                .stream()
+                .map(course -> mapper.map(course, GroupDTO.class))
+                .collect(toList());
+    }
+
+    @Override
+    public GroupDTO createGroup(GroupDTO groupDTO) {
+        Group group = mapper.map(groupDTO, Group.class);
+        addGroup(group);
+        return groupDTO;
+    }
+
+    @Override
+    public GroupDTO findDTOById(Long groupId) {
+        return mapper.map(findById(groupId), GroupDTO.class);
     }
 }
