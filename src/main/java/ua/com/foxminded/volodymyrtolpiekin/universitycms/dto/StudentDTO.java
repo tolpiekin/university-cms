b70052bb26b5bae60d964d8d7c56605d2f6dc420
Volodymyr Tolpiekin <tolpekin.vladimir@gmail.com>
2023-01-17 16:42:34 +0200

    Added StudentDTO.

---
+++ src/main/java/ua/com/foxminded/volodymyrtolpiekin/universitycms/dto/StudentDTO.java
@@ -0,0 +1,14 @@
+package ua.com.foxminded.volodymyrtolpiekin.universitycms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ua.com.foxminded.volodymyrtolpiekin.universitycms.models.Group;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentDTO {
+    private Long id;
+    private Group group;
+}
